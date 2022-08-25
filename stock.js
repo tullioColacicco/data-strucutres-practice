f432b3382a967a4d5ec387dd6043062ab424af75
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2022-08-24 20:19:27 -0400

    done

--- stock.js
+++ stock.js
@@ -1,59 +1,55 @@
-function stocks(arr){
-let stack = []
-let answer = [...arr]
-for(i=0;i<arr.length;i++){
-    let price = arr[i]
-    let span = 0
-    console.log(stack,arr[i]);
-    
-    while(stack.lengh > 0&& stack[stack.length-1][0]>price){
-        
-        let [price,index] = stack.pop()
-        answer[index]= i - index
-        span+=1
+function stocks(arr) {
+  let stack = [];
+  let answer = [...arr];
+  for (i = 0; i < arr.length; i++) {
+    let price = arr[i];
+    let span = 0;
+    console.log(stack, arr[i]);
+
+    while (stack.lengh > 0 && stack[stack.length - 1][0] > price) {
+      let [price, index] = stack.pop();
+      answer[index] = i - index;
+      span += 1;
     }
-    stack.push([price,i])
-    console.log(stack[stack.length-1][0]);
+    stack.push([price, i]);
+    console.log(stack[stack.length - 1][0]);
+  }
+
+  return answer;
 }
 
-return answer
+console.log(stocks([100, 80, 60, 70, 60, 75, 85]));
 
-}
-
-
-                              1               1
-console.log(stocks([ 100,80, 60, 70, 60, 75, 85]));
-
-var dailyTemperatures = function(temperatures) {
-    let stack = []
-    let output = [...temperatures]
-for(i=0;i<temperatures.length;i++){
-    while(stack.length && stack[stack.length-1][0]>temperatures[i]){
-        let [temp,index] = stack.pop()
-        output[index]=i-index
+var dailyTemperatures = function (temperatures) {
+  let stack = [];
+  let output = [...temperatures];
+  for (i = 0; i < temperatures.length; i++) {
+    while (stack.length && stack[stack.length - 1][0] > temperatures[i]) {
+      let [temp, index] = stack.pop();
+      output[index] = i - index;
     }
 
-    stack.push([temperatures[i],i])
-}
-return output
-  };
+    stack.push([temperatures[i], i]);
+  }
+  return output;
+};
 
+console.log(
+  dailyTemperatures((temperatures = [73, 74, 75, 71, 69, 72, 76, 73]))
+);
 
-  console.log(dailyTemperatures(temperatures = [73,74,75,71,69,72,76,73]));
-
-  StockSpanner = function(arr) {
-    let stack=[]
-      let span = 1;
-      let output = [...arr]
-      for(i=0;i<arr.length;i++){
-      while (stack.length > 0 && stack[stack.length-1].price <= arr[i]) {
-          let popped = stack.pop();
-          output[popped.span]=i-popped.span
-          span += popped.span;
-          
-      }
+StockSpanner = function (arr) {
+  let stack = [];
+  let span = 1;
+  let output = [...arr];
+  for (i = 0; i < arr.length; i++) {
+    while (stack.length > 0 && stack[stack.length - 1].price <= arr[i]) {
+      let popped = stack.pop();
+      output[popped.span] = i - popped.span;
+      span += popped.span;
     }
-      stack.push({price: arr[i], span: span});
-      return output;
-    };
-    console.log(StockSpanner([ 100,80, 60, 70, 60, 75, 85]));
+  }
+  stack.push({ price: arr[i], span: span });
+  return output;
+};
+console.log(StockSpanner([100, 80, 60, 70, 60, 75, 85]));
