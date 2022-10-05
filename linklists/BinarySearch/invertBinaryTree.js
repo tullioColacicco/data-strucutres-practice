659b097784a7b2b0c5030b11b9bea9fb1c0c56c5
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2022-10-04 23:50:23 -0400

    stack version

--- linklists/BinarySearch/invertBinaryTree.js
+++ linklists/BinarySearch/invertBinaryTree.js
@@ -1,10 +1,26 @@
+// var invertTree = (root) => {
+//   console.log(root)
+//   const isBaseCase = root === null;
+//   if (isBaseCase) return root;
+
+//   return dfs(root);
+// };
+//stack
 var invertTree = (root) => {
-  const isBaseCase = root === null;
-  if (isBaseCase) return root;
-
-  return dfs(root);
+  let stack = [root];
+  while (stack.length > 0) {
+    console.log(stack);
+    let node = stack.pop();
+    if (node != null) {
+      let temp = node.left;
+      node.left = node.right;
+      node.right = temp;
+      stack.push(node.right);
+      stack.push(node.left);
+    }
+  }
+  return root;
 };
-
 const dfs = (root) => {
   const left = invertTree(root.left);
   const right = invertTree(root.right);
@@ -56,10 +72,20 @@ BinarySearch.prototype.insert = function (value) {
 };
 
 let tree = new BinarySearch();
+function insert(arr) {
+  for (item of arr) {
+    tree.insert(item);
+  }
+  return tree;
+}
 tree.insert(4);
 tree.insert(5);
 tree.insert(7);
 tree.insert(2);
 tree.insert(1);
 tree.insert(3);
-console.log(tree.root.left);
+// console.log(insert([4,2,7,1,3,6,9]))
+console.log(tree.root.right);
+// let rev = invertTree(tree)
+console.log(invertTree(tree.root));
+// console.log(rev.root.right)
