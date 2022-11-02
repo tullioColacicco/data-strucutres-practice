b71fef50141fb08b7f1ee64ed0fc34064eba92e7
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2022-11-01 23:52:26 -0400

    count oranges.js

--- .vscode/graphs/hasPath.js
+++ .vscode/graphs/hasPath.js
@@ -37,3 +37,27 @@ let adjList = {
 };
 
 console.log(hasPath(adjList, "f", "k"));
+
+var orangesRotting = function (grid) {
+  let count = 0;
+  let row = grid.length;
+  let col = grid[0].length;
+  for (i = 0; i < row; i++) {
+    for (c = 0; c < col.length; c++) {
+      if (grid[i][c] === 2) count += dfs(i, c, grid);
+    }
+  }
+  return count;
+};
+
+function dfs(i, c, grid) {
+  if (grid[i + 1][c] && grid[i + 1][c] != 1) grid[i + 1][c] = 2;
+  if (grid[i - 1][c] && grid[i - 1][c] != 1) grid[i - 1][c] = 2;
+  if (grid[i][c + 1] && grid[i][c + 1] != 1) grid[i][c + 1] = 2;
+  if (grid[i][c - 11] && grid[i][c - 1] != 1) grid[i][c - 1] = 2;
+  dfs(i + 1, c, grid);
+  dfs(i - 1, c, grid);
+  dfs(i, c + 1, grid);
+  dfs(i, c - 1, grid);
+  return 1;
+}
