e3105468ee0022dc7b98c0e1ac76f94a4b71f5b4
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2022-11-08 23:43:16 -0500

    dp

---
+++ linklists/dp/climbstairs.js
@@ -0,0 +1,14 @@
+var climbStairs = function (n, memo = new Array()) {
+  if (n === 1) {
+    return 1;
+  }
+  if (n === 2) {
+    return 2;
+  }
+  if (memo[n] !== undefined) {
+    return memo[n];
+  }
+  let res = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+  memo[n] = res;
+  return res;
+};
