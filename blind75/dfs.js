afcceb3909b0ff88de684b19b4c78e3bee2dac2f
tullioColacicco <56273725+tullioColacicco@users.noreply.github.com>
2022-12-09 18:27:44 -0500

    blind75

--- blind75/dfs.js
+++ blind75/dfs.js
@@ -30,21 +30,77 @@ var twoSum = function (nums, target) {
   }
 };
 
-var isPalindrome = function (s) {
-  s = s.replace(/[^0-9a-zA-Z]+/gim, "");
-  s = s.toLowerCase();
-  var l = 0,
-    r = s.length - 1;
-  console.log(s);
+// var isPalindrome = function (s) {
+//   s = s.replace(/[^0-9a-zA-Z]+/gim, "");
+//   s = s.toLowerCase();
+//   var l = 0,
+//     r = s.length - 1;
+//   console.log(s);
 
-  while (l < r) {
-    if (s.charAt(l) != s.charAt(r)) {
-      return false;
-    }
-    l++;
-    r--;
+//   while (l < r) {
+//     if (s.charAt(l) != s.charAt(r)) {
+//       return false;
+//     }
+//     l++;
+//     r--;
+//   }
+//   return true;
+// };
+
+function isPalindrome(s) {
+  let string = "";
+  for (item of s) {
+    console.log(item);
+    item = item.toLowerCase();
+    if (item.toUpperCase() != item) string += item;
   }
-  return true;
-};
+  let copy = "";
+  for (i = string.length - 1; i >= 0; i--) {
+    copy += string[i];
+  }
+  return string === copy;
+}
+function Node(value, next) {
+  this.value = value;
+  this.next = null;
+}
+let list1 = [1, 2, 4];
+let list2 = [1, 3, 4];
+function mergeTwoSortedLists(l1, l2) {
+  let list1 = makeList(l1);
+  let list2 = makeList(l2);
+  let head = new Node(0);
+  //  console.log(list1.next)
+  let current = head;
+  while (list1 != null && list2 != null) {
+    if (list1.value <= list2.value) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  //  current.next = list1 || list2
+  console.log(head.next);
+  return head;
+}
+
+function makeList(list) {
+  let head = new Node(0);
+  let current = head;
+  for (item of list) {
+    let newNode = new Node(item);
+    current.next = newNode;
+    current = current.next;
+  }
+  return head.next;
+}
+
+console.log(makeList(list1));
+console.log(mergeTwoSortedLists(list1, list2));
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 console.log(twoSum((nums = [2, 7, 11, 15]), (target = 9)));
 console.log(dfs(graph, "a", (map = {}), "d"));
